c9a80ec115674444798334cdb05ce1a1fbb853da
valeriyaduran <valeriya.duran@gmail.com>
2022-03-23 21:59:51 +0300

    SECRET_KEY generated, requirements freezed

--- stepik_vacancies/settings.py
+++ stepik_vacancies/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e#c-$tfu0g)r^$s7_p8s3bx6(n2kdxq-w%9k=(@7zwa^8qgrg3'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -41,6 +44,7 @@ INSTALLED_APPS = [
     'phonenumber_field',
     'phonenumbers',
     'crispy_forms',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
